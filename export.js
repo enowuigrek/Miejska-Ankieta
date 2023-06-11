ae5f88c1d0f03900958599d4b608d56d3ae9f67e
Łukasz Nowak <99438005+enowuigrek@users.noreply.github.com>
2023-06-11 16:48:01 +0200

    add export from firebase

---
+++ export.js
@@ -0,0 +1,29 @@
+const admin = require('firebase-admin');
+
+const fs = require('fs');
+
+const serviceAccount = require('./my-firebase-adminsdk.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+
+async function exportData() {
+    try {
+        const snapshot = await db.collection('answers').get();
+        const answers = [];
+
+        snapshot.forEach((doc) => {
+            answers.push(doc.data());
+        });
+
+        fs.writeFileSync('answers.json', JSON.stringify(answers, null, 2));
+        console.log('Data successfully exported!');
+    } catch (error) {
+        console.error('Error exporting data: ', error);
+    }
+}
+
+exportData();
