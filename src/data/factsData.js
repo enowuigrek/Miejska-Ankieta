cf789222f085fc5f9c5cec16b0f6d56f400ca385
Łukasz Nowak <99438005+enowuigrek@users.noreply.github.com>
2023-06-12 00:21:31 +0200

    add some facts

--- src/data/factsData.js
+++ src/data/factsData.js
@@ -3,5 +3,11 @@ export const FACTS_DATA = [
 
     'Właśnie niedawno, w 2023 roku, naukowcy odkryli nowy gatunek ptaka na odległej wyspie w Oceanii. Ptak ten został nazwany "koralezia złotooka" (Auroravis aureoculus) ze względu na jaskrawo złote pióra wokół oczu. Co czyni to odkrycie szczególnie interesującym, to fakt, że ptak ten jest w stanie emitować delikatne świetlne sygnały za pomocą tych złotych pierzastych obszarów wokół oczu. Badacze sugerują, że koralezia złotooka może wykorzystywać to światło jako formę komunikacji w ciemnościach tropikalnego lasu, w którym żyje. Odkrycie to stanowi kolejny dowód na niesamowitą różnorodność i adaptacje przyrody, które nadal czekają na nasze odkrycie.',
 
-    'Ciekawostka 3',
+    'Trytytka',
+
+    'Piwne oczy',
+
+    'Guiness',
+
+    
 ];
