ba9165e475b489743ae316e84c8d88abefa1bb8f
Łukasz Nowak <99438005+enowuigrek@users.noreply.github.com>
2023-06-13 23:29:28 +0200

    add more facts

--- src/data/factsData.js
+++ src/data/factsData.js
@@ -1,11 +1,59 @@
 export const FACTS_DATA = [
     'Zielone oliwki rosną na tych samych drzewach co czarne. To te same oliwki tylko, mniej dojżałe.',
 
-    'Trytytka, czyli opaska zaciskowa, która ma bardzo wiele zastosowań i możnia dzięki niej naprawić prawie wszystko, swą nazwę wzięła od firmy, króra wprowadziła ją na rynek - "Trytyt".',
+    'Trytytka, czyli opaska zaciskowa, która ma bardzo wiele zastosowań i dzięki której można naprawić prawie wszystko, swą nazwę wzięła od firmy, która wprowadziła ją na rynek - "Trytyt".',
 
-    'Dawno temu piwo miało różne odcienie koloru brązowego. Nie było piw jasnych o złocistej barwie, ani ciemnych, gdyż produkcja słodu nie pozwalała na sterowanie kolorem. Było po prostu brązowe. Dlatego do dziś brązowe oczy nazywamy piwnymi.',
+    'Dawno temu piwo miało różne odcienie koloru brązowego. Nie było piw jasnych o złocistej barwie, ani ciemnych, gdyż produkcja słodu nie pozwalała na precyzyjne sterowanie kolorem. Było po prostu brązowe. Dlatego do dziś, brązowe oczy nazywamy piwnymi.',
 
-    'W latach 50-tych zeszłego wieku, w browarze Guiness, wśród pracowników miał miejsce spór, o to który ptak jest najszybszym łownym ptakiem Europy. DOprowadził on do myśli, że nie ma dostępnego źródła, które mogłoby rozstrzygnąć tego typu spory, co zapoczątkowało pomysł utworzenia księgi rekordów. Tak powstała księga Guinessa.',
+    'W latach 50. zeszłego wieku w browarze Guinness wśród pracowników miał miejsce spór o to, który ptak jest najszybszym łownym ptakiem Europy. Doprowadził on do myśli, że nie ma dostępnego źródła, które mogłoby rozstrzygnąć tego typu spory, co zapoczątkowało pomysł utworzenia księgi rekordów. Tak powstała księga Guinnessa.',
 
     'Żaby nie piją wody. Pobierają ją przez skórę.',
+
+    'Ciało strusia jest tak duże, że jego mózg jest mniejszy od jednego z jego oczu.',
+
+    'Błyskawica jest 5 razy gorętsza niż powierzchnia słońca.',
+
+    'Koliberki są jedynymi ptakami, które potrafią latać do tyłu.',
+
+    'Woda jest jedyną substancją na Ziemi, która występuje naturalnie w trzech stanach skupienia: ciekłym, stałym i gazowym.',
+
+    'Jedno drzewo produkuje na tyle tlenu, ile potrzeba do oddychania przez 2 osoby.',
+
+    'Mrówki nigdy nie śpią.',
+
+    'W 2006 roku, Nasa wysłała płyty CD ze 90 minutami muzyki Beatlesów w kosmos.',
+
+    'Ośmiornice mają trzy serca.',
+
+    'Najwyższa temperatura jaka kiedykolwiek została odnotowana na Ziemi to 56.7 stopni Celsjusza w Dolinie Śmierci, USA.',
+
+    'Biała czekolada nie zawiera ziaren kakaowca. Technicznie nie powinna się nazywać czekoladą, tak jak kawa zbożowa kawą',
+
+    'Żaden kawałek papieru nie może być złożony na pół więcej niż siedem razy. Challenge accepted?',
+
+    'Słońce jest niebieskie z kosmosu, a nie żółte.',
+
+    'Delfiny są jednymi z niewielu zwierząt, które potrafią rozpoznawać się nawzajem w lustrze.',
+
+    'Jeden z najdłużej żyjących organizmów na świecie to ośmiornica o imieniu Ming, która dożyła 507 lat.',
+
+    'Jedna trzecia żywności produkowanej na świecie jest marnowana.',
+
+    'Koala śpi średnio 20 godzin dziennie.',
+
+    'Niedźwiedzie polarne są leworęczne.',
+
+    'Tęcza jest kołem pełnym, ale zazwyczaj widzimy tylko jego półokrągły fragment na niebie.',
+
+    'Statystycznie osoby praworęczne piją więcej kawy niż osoby leworęczne.',
+
+    'Nietoperze są jedynymi ssakami, które potrafią latać i tak naprawdę to widzą. To, że są ślepe, to mit. Używają echolokacji, ale wzroku też.',
+
+    'Słoń to jedyny ssak, który nie potrafi skakać',
+
+    'Nie da się kogoś uśpić chloroforem, przykłądając nasączoną nim szmatkę, czy chusteczkę, jak często widzimy w filmach. Proces działania chloroformu jako środka znieczulającego jest bardziej skomplikowany i wymaga dłuższego kontaktu oraz odpowiednich stężeń.',
+
+    'Tłumik w pistoletach nie sprawia, że strzały stają się całkowicie bezszelestne ani niemożliwe do wykrycia. Służy głównie ochronie słuchu strzelca oraz redukcji hałasu dla osób w pobliżu. Psiu psiu w filmach to mit. Swoją drogą jak dodatkowa nasadka na lufie, miałaby wyciszyć wybuchający proch? Ona działa głównie na redukcję hałasu wytwarzanego przez gaz wydobywający się z lufy.',
+
+    'Czerwona papryka zawiera więcej witaminy C niż cytryna. Na przykład, 100 gramów czerwonej papryki może dostarczyć około 128 mg witaminy C, podczas gdy ta sama ilość cytryny zawiera około 53 mg witaminy C.',
 ];
