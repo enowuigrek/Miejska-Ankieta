2c03d885d66bda7ac88ebdb3318afadfa3175b12
Łukasz Nowak <99438005+enowuigrek@users.noreply.github.com>
2023-06-26 22:33:12 +0200

    fix favicon

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -5,7 +5,7 @@ const PageNotFound = () => {
     return (
         <div className='not-found-container'>
             <h1>404</h1>
-            <p>Sorki, coś poszłonie tak. Chyba, że to Ty kombinujesz.</p>
+            <p>Sorki, coś poszło nie tak. Chyba, że to Ty kombinujesz.</p>
         </div>
     );
 };
