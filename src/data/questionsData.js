76c515b4708256ba3baf53507b362118fef626ef
Łukasz Nowak <99438005+enowuigrek@users.noreply.github.com>
2024-03-30 17:51:32 +0100

    question about steps

--- src/data/questionsData.js
+++ src/data/questionsData.js
@@ -114,4 +114,14 @@ export const QUESTIONS_DATA = {
             { id: 'od ściany', label: 'OD ściany' },
         ],
     },
+
+    kroki: {
+        questionText: 'Ile kroków dziennie robisz?',
+        options: [
+            { id: 'mniej niż 6 000', label: 'mniej niż 6 000' },
+            { id: 'między 6 000 a 10 000', label: 'między 6 000 a 10 000' },
+            { id: 'między 10 000 a 15 000', label: 'iędzy 10 000 a 15 000' },
+            { id: 'więcej niż 10 000', label: 'więcej niż 10 000' },
+        ],
+    },
 };
