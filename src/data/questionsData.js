31204a1adac2112431d56d83c4482f3999ffe54b
Łukasz Nowak <99438005+enowuigrek@users.noreply.github.com>
2024-03-30 17:58:07 +0100

    typo

--- src/data/questionsData.js
+++ src/data/questionsData.js
@@ -120,7 +120,7 @@ export const QUESTIONS_DATA = {
         options: [
             { id: 'mniej niż 6 000', label: 'mniej niż 6 000' },
             { id: 'między 6 000 a 10 000', label: 'między 6 000 a 10 000' },
-            { id: 'między 10 000 a 15 000', label: 'iędzy 10 000 a 15 000' },
+            { id: 'między 10 000 a 15 000', label: 'między 10 000 a 15 000' },
             { id: 'więcej niż 15 000', label: 'więcej niż 15 000' },
         ],
     },
